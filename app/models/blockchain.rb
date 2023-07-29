c06804fd30eba3a4ed4c88c91d6b09c90a7f6666
EEUAAAIA <eeuaaaia168@outlook.fr>
2023-07-29 05:21:34 -0400

    last commit

--- app/models/blockchain.rb
+++ app/models/blockchain.rb
@@ -1,6 +1,7 @@
 class Blockchain < ApplicationRecord
-  has_many :wallets
+  validates :name, presence: true, uniqueness: true
 
+  has_many :wallets
   has_many :user_blockchains
   has_many :users, through: :user_blockchains
 end
--- app/models/user_blockchain.rb
+++ app/models/user_blockchain.rb
@@ -1,5 +1,7 @@
 class UserBlockchain < ApplicationRecord
 
+#  validates :blockchain, uniqueness: { scope: :user_id, message: "Cette blockchain a déjà été ajoutée par l'utilisateur" }
+
   belongs_to :user
   belongs_to :blockchain
   accepts_nested_attributes_for :blockchain
