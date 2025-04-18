83469de66348aedc838908ffbbb24a67f9756f83
EEUAAAIA <eeuaaaia168@outlook.fr>
2025-04-18 15:28:41 -0400

    Create version en.yml/ fr.yml

--- config/routes.rb
+++ config/routes.rb
@@ -4,22 +4,24 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 
-  devise_for :users
+  scope "(:locale)", locale: /en|fr/ do
+    
+    devise_for :users
 
-  devise_scope :user do
-     get '/users/sign_out' => 'devise/sessions#destroy'
+    devise_scope :user do
+       get '/users/sign_out' => 'devise/sessions#destroy'
+    end
+
+    root "home#index"
+
+    resources :addresses
+    resources :blockchains
+    resources :wallets
+    resources :wallet_adresses
+
+    resources :user_blockchains
+    resources :user_wallets
+    resources :user_addresses
+
   end
-
-  root "home#index"
-
-  resources :addresses
-  resources :blockchains
-  resources :wallets
-  resources :wallet_adresses
-
-  resources :user_blockchains
-  resources :user_wallets
-  resources :user_addresses
-
-
 end
